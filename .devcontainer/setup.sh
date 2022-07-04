78ef2308055b527a63483ed1bd2fc227f5b010f8
Mujahid Ishtiaq <mujahid158@gmail.com>
2022-07-05 01:45:13 +0500

    Testing Codespaces devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+SLUG=simple-podcasting
+PROJECT_TYPE=plugin
+
+if [[ ! -z "$CODESPACE_NAME" ]]
+then
+	SITE_HOST="https://${CODESPACE_NAME}-8080.githubpreview.dev"
+else
+	SITE_HOST="http://localhost:8080"
+fi
+
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>setup.log 2>&1
+
+# Prepare a nice name from project name for the site title.
+function getTitleFromSlug()
+{
+    local _slug=${SLUG//-/ }
+    local __slug=${_slug//_/ }
+    local ___slug=( $__slug )
+    echo "${___slug[@]^}"
+}
+
+source ~/.bashrc
+
+# Install node
+nvm install 10
+nvm alias default 10
+
+# Install dependencies
+cd /var/www/html/wp-content/${PROJECT_TYPE}s/${SLUG}/
+npm i && npm run build
+composer i
+
+# Install WordPress and activate the plugin/theme.
+cd /var/www/html/
+echo "Setting up WordPress at $SITE_HOST"
+wp db reset --yes
+wp core install --url="$SITE_HOST" --title="$(getTitleFromSlug) Development" --admin_user="admin" --admin_email="admin@example.com" --admin_password="password" --skip-email
+
+echo "Activate $SLUG"
+wp $PROJECT_TYPE activate $SLUG
